5fdeae60d1db46a5d0964a15b6667366d74c5d5a
1066875821@qq.com <wujun3235434925>
2017-06-09 13:50:09 +0800

    test

---
+++ Test/T000.py
@@ -0,0 +1,23 @@
+# _*_ coding: UTF-8 _*_
+"""
+python3.0 scripts test
+"""
+#
+print('python3.0 scripts test')
+#
+#################################################################################
+
+while True:
+    reply = input('Enter text:')
+    if reply == 'stop': break
+    try:
+        num = int(reply)
+    except:
+        print
+        print('bad!' * 8)
+    else:
+        print(int(reply) ** 2)
+print('Bye')
+
+a = 8
+b = 9
---
+++ Test/T002.py
@@ -0,0 +1,10 @@
+# _*_ coding: UTF-8 _*_
+# 时间: 2017 - 06 - 09
+"""
+python 学习手册(第四版）示例内容
+"""
+#
+import platform
+print(platform.python_version())
+print('python 学习手册(第四版）示例内容')
+#
