2472ff9e7054c38bc7c54c6a36057c2e98219aa9
1066875821@qq.com <wujun3235434925>
2017-06-09 16:48:19 +0800

    test

--- Test/T002.py
+++ Test/T002.py
@@ -26,6 +26,6 @@ spam = 'Spam' #基本形式
 spam,ham = 'yum','YUM' #元组赋值运算（位置性）
 [spam, ham] = ['yum','YUM'] #列表复制运算（位置性）
 a,b,c,d = 'spam' #序列复制运算，通用性
-a, *d = 'spam' #扩展的序列捷豹（Python 3.0)
+a, *d = 'spam' #扩展的序列解包（Python 3.0)
 spam = ham = 'lunch' #多目标赋值运算
 spams += 42 #增强赋值运算（相当于 spams = spams + 42)
