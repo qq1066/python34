374b4d52b19945327e0ab6fe3c8a89d9ed888a21
qq1066 <1066875821@qq.com>
2017-07-16 12:00:58 +0800

    test

--- Test/T000.py
+++ Test/T000.py
@@ -1,13 +1,10 @@
+#!/bin/env python
 # _*_ coding: UTF-8 _*_
-"""
-python3.0 scripts test
-"""
-#
-print('python3.0 scripts test')
-#
-#################################################################################
+#python Version: 3.4.4
+################################################################################
+a = 'iron man'
+b = 'iron man'
+print (a is b)
 
-nudge = 1
-wink =2
-A, B = nudge, wink
-print(A, B)
+
+
